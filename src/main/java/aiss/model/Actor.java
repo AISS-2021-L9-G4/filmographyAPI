0a79d7e2c48100ab83d26912145602f53979a4a0
ijordano <n.jordano10@gmail.com>
2021-06-05 21:02:30 +0200

    Film y Actor según esquemas

--- src/main/java/aiss/model/Actor.java
+++ src/main/java/aiss/model/Actor.java
@@ -2,4 +2,40 @@ package aiss.model;
 
 public class Actor {
 
+	private String id;
+	private String name;
+	private String nationality;
+	private String age;
+
+	public String getId() {
+	return id;
+	}
+	
+	public void setId(String id) {
+	this.id = id;
+	}
+	
+	public String getName() {
+	return name;
+	}
+	
+	public void setName(String name) {
+	this.name = name;
+	}
+	
+	public String getNationality() {
+	return nationality;
+	}
+	
+	public void setNationality(String nationality) {
+	this.nationality = nationality;
+	}
+	
+	public String getAge() {
+	return age;
+	}
+	
+	public void setAge(String age) {
+	this.age = age;
+	}
 }
--- src/main/java/aiss/model/Film.java
+++ src/main/java/aiss/model/Film.java
@@ -1,5 +1,51 @@
 package aiss.model;
 
-public class Film {
+import java.util.List;
 
+public class Film {
+	private String id;
+	private String name;
+	private String category;
+	private Integer year;
+	private List<Actor> cast = null;
+	
+	public String getId() {
+	return id;
+	}
+
+	public void setId(String id) {
+	this.id = id;
+	}
+
+	public String getName() {
+	return name;
+	}
+
+	public void setName(String name) {
+	this.name = name;
+	}
+
+	public String getCategory() {
+	return category;
+	}
+
+	public void setCategory(String category) {
+	this.category = category;
+	}
+
+	public Integer getYear() {
+	return year;
+	}
+
+	public void setYear(Integer year) {
+	this.year = year;
+	}
+
+	public List<Actor> getCast() {
+	return cast;
+	}
+
+	public void setCast(List<Actor> cast) {
+	this.cast = cast;
+	}
 }
